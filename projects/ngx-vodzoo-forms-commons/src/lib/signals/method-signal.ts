b69abab437bf499fe662368a41815d7ecf507f10
Vodzoo <32583728+Vodzoo@users.noreply.github.com>
2025-03-06 21:56:41 +0100

    signal method args

--- projects/ngx-vodzoo-forms-commons/src/lib/signals/method-signal.ts
+++ projects/ngx-vodzoo-forms-commons/src/lib/signals/method-signal.ts
@@ -2,28 +2,31 @@ import {Signal} from "@angular/core";
 import {filter, isObservable, Observable, of, switchMap, tap} from "rxjs";
 import {toObservable, toSignal} from "@angular/core/rxjs-interop";
 
-export function methodSignal<T, R, P extends R = R>(
-  params: Signal<NonNullable<T> | undefined>,
-  computation: (args: { methodParams: NonNullable<T>, previousMethodParams?: T, previousMethodValue?: P }) => Observable<R> | R
-): Signal<R | undefined> {
+export function methodSignal<T, R, P extends R = R>(args: MethodSignalArgs<T, R, P>): Signal<R | undefined> {
   let previousMethodValue: P | undefined;
   let previousMethodParams: NonNullable<T> | undefined;
   return toSignal(
-    toObservable(params)
+    toObservable(args.params)
       .pipe(
         filter(isNonNullable),
         switchMap(methodParams => {
-          const comp: Observable<R> | R = computation({methodParams, previousMethodParams, previousMethodValue});
+          const comp: Observable<R> | R = args.computation({methodParams, previousMethodParams, previousMethodValue});
           return isObservable(comp) ? comp : of(comp);
         }),
         tap(value => {
           previousMethodValue = value as P;
-          previousMethodParams = params();
+          previousMethodParams = args.params();
         })
-      )
+      ),
+    { rejectErrors: true }
   );
 }
 
 export function isNonNullable<T>(obj: T): obj is NonNullable<T> {
   return obj !== undefined && obj !== null;
+}
+
+export interface MethodSignalArgs<T, R, P extends R = R> {
+  params: Signal<NonNullable<T> | undefined>;
+  computation: (args: { methodParams: NonNullable<T>, previousMethodParams?: T, previousMethodValue?: P }) => Observable<R> | R;
 }
