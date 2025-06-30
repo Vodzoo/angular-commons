5f322ebb3ab079ffa1b7929b81ec6ddbc949e364
Vodzoo <32583728+Vodzoo@users.noreply.github.com>
2025-06-30 22:27:06 +0200

    markAllAsTouched function

---
+++ projects/ngx-vodzoo-forms-commons/src/lib/markAllAsTouched.ts
@@ -0,0 +1,12 @@
+import {AbstractControl, FormArray, FormControl, FormGroup} from "@angular/forms";
+
+export function markAllAsTouched(abstractControl: AbstractControl): void {
+  if (abstractControl instanceof FormGroup || abstractControl instanceof FormArray) {
+    Object.values(abstractControl.controls).forEach((control: AbstractControl) => {
+      markAllAsTouched(control);
+    })
+  } else if (abstractControl instanceof FormControl) {
+    abstractControl.markAsTouched({onlySelf: true});
+    abstractControl.updateValueAndValidity({onlySelf: true});
+  }
+}
--- projects/ngx-vodzoo-forms-commons/src/public-api.ts
+++ projects/ngx-vodzoo-forms-commons/src/public-api.ts
@@ -9,6 +9,7 @@ export * from './lib/directives/form-events.directive';
 export * from './lib/directives/form-field.directive';
 export * from './lib/disableWithContext';
 export { getConfig, getDefaultConfig, getConfigField, Paths } from './lib/formConfig';
+export * from './lib/markAllAsTouched';
 export { DEFAULT_MERGE_CONFIG, MERGE_CONFIG, MergeConfig } from './lib/mergeDeep';
 export * from './lib/services/form.service';
 export * from './lib/signals/method-signal';
